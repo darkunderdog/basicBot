e8655f49bddf3b8b864d3f19533139bd6fb7b0cb
DarkunderdoG <darkunderdog@gmail.com>
2015-04-18 17:50:41 -0600

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1135,7 +1135,7 @@
                 }
                 
                 if (chat.message.indexOf('!kitty') >= 0 || chat.message.indexOf('!meow') >= 0)  {
-                    if (userPerm < 2) {
+                    if (userPerm < 10) {
                         var u = basicBot.userUtilities.lookupUser(chat.uid);
                         var diff = (60*10) - (((Date.now() - u.lastGif) / 1000) | 0);
         		var minutes = (diff / 60) | 0;
