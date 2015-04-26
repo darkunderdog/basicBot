26d7220264250100bc68571af9af17ef132af9e7
DarkunderdoG <darkunderdog@gmail.com>
2015-04-26 10:02:50 -0600

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2625,6 +2625,19 @@
                     }
                 }
             },
+            
+            CurrentWinnerCommand: {
+                command: 'currentwinner',
+                rank: 'user',
+                type: 'startsWith',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+			API.sendChat('/me Current Winner: ' + basicBot.settings.motd);
+                         }
+                }
+            },
 
             moveCommand: {
                 command: 'move',
