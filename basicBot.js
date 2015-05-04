26037adf988c4a7440b92835f772c6827dee34e2
DarkunderdoG <darkunderdog@gmail.com>
2015-05-03 22:01:20 -0600

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -12,6 +12,10 @@
             id = API.getUser().id;
         }
         var wl = API.getWaitList();
+        for(var i = 0; i < wl.length; i++){		
+            if(wl[i].id === id){		
+                return i;		
+            }
         }
         return -1;
     };
