6234ffff1498f12be59c5d19bb7dc9ca5cde61d7
DarkunderdoG <darkunderdog@gmail.com>
2015-07-26 08:25:21 -0600

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2768,9 +2768,9 @@
                 }
             },
 			
-			    CWMinAudiencecmd: {
+	    CWMinAudiencecmd: {
                 command: 'cwm',
-                rank: 'manager',
+                rank: 'bouncer',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
